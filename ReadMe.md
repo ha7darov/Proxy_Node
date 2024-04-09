b66637b44602e2e1a0a7f10a01c7ee36e0ae9167
ha7darov <vi.yeoun.vi@gmail.com>
2024-04-09 19:21:35 +0500

    Some End

---
+++ ReadMe.md
@@ -0,0 +1,27 @@
+# <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg" width="40px"> Proxy NodeJS
+
+---
+
+## [Proxy Server Live]()
+
+---
+# How To Run
+
+- ## 1. ___Package Download___
+```bash
+npm init
+```
+
+```bash
+npm i
+```
+
+- ## 2. ___Run___
+```bash
+npm run dev
+```
+
+## ___Watch Localhost___
+```bash
+http://localhost:9000/
+```
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const puppeteer = require('puppeteer')
+const replace = require('absolutify')
 const path = require('path')
+const { log } = require('console')
 
 const app = express()
 
@@ -9,4 +12,29 @@ app.get('/', (req, res) => {
     res.sendFile('/index.html')
 })
 
-app.listen(3000, () => console.log('Server is running on port 3000'))
+app.get('/proxy', async (req, res) => {
+    const { url } = req.query
+
+    if(!url){
+        return res.send('No url provided')
+    } else{
+        try {
+            const browser = await puppeteer.launch()
+            const page = await browser.newPage()
+
+            await page.goto(`https://${url}`)
+
+            let document = await page.evaluate(() => document.documentElement.outerHTML)
+            document = replace(document, `/proxy?url=${url.split('/')[0]}`)
+
+            await browser.close()
+
+            return res.send(document)
+        } catch (err) {
+            console.log(err);
+            return res.send(err)
+        }
+    }
+})
+
+app.listen(9000, () => console.log('Server is running on port 9000 :) http://localhost:9000/'))
